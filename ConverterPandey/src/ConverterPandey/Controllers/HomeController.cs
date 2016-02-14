91e543038929d78322f5b33b980651bc1b4ba90c
s525078 <s525078@mail.nwmissouri.edu>
2016-02-14 17:03:04 -0600

    Done Committed

---
+++ ConverterPandey/src/ConverterPandey/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Mvc;
+using ConverterPandey.Models;
+
+namespace ConverterPandey.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Converter App by Pandey";
+            ViewData["Result"] = "";
+
+            Converter converter = new Converter();
+            return View(converter);
+        }
+
+        public IActionResult Convert(Converter converter)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Title"] = "Converted by Pandey";
+                ViewData["Result"] = "Temperature in C = " +
+                    (int)((converter.Temperature_F - 32) * 5.0 / 9.0);
+            }
+            return View("Index", converter);
+        }
+
+    }
+}
---
+++ ConverterPandey/src/ConverterPandey/Models/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConverterPandey.Models
+{
+    public class Converter
+    {
+        public int ConverterID { get; set; }
+
+        [Display(Name = "ZIP Code")]
+        [RegularExpression(@"^\d{5}(?:[-\s]\d{4})?$", ErrorMessage = "Please enter a valid US ZIP code")]
+        public int Zipcode { get; set; } = 64468;
+
+        [Required]
+        [Range(-12, 120)]
+        [Display(Name = "Temperature (F)")]
+        public int Temperature_F { get; set; } = 72;
+    }
+}
